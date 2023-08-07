c3f7915c82db121811477d8bd924c321159b3457
DongCheol PARK <pksam5046@naver.com>
2023-08-07 15:50:50 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/dongcheol/Login/Login.jsx
+++ src/pages/dongcheol/Login/Login.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import React from "react";
 import "./Login.scss";
 import { Link } from "react-router-dom";
 import Nav from "../components/Nav/Nav";
 
 export default function LoginDongcheol() {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const saveUserId = (e) => {
+    setId(e.target.value);
+  };
+  const saveUserPw = (e) => {
+    setPw(e.target.value);
+  };
+
   return (
     <>
       <Nav />
@@ -12,20 +23,24 @@ export default function LoginDongcheol() {
           <div className="logo">
             <img src="/images/dongcheol/instagram_logo.png" alt="instagram" />
           </div>
-          <form action="/login" method="post">
+          <form action="/login" method="POST">
             <input
               type="text"
               name="username"
               placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={saveUserId}
+              value={id}
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="비밀번호"
+              onChange={saveUserPw}
+              value={pw}
               required
             />
             <Link to="/dongcheol-main">
-              <input
-                type="password"
-                name="password"
-                placeholder="비밀번호"
-                required
-              />
               <input type="submit" value="로그인" />
             </Link>
           </form>
