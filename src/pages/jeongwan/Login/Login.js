d34def941cc730a94ba516edc7d5bb867bc6461e
wanwork <wjdqo9705@gmail.com>
2023-08-07 18:56:42 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jeongwan/Login/Login.js
+++ src/pages/jeongwan/Login/Login.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  // 메인으로 가는 함수
   const navigate = useNavigate();
 
   const goToMain = () => {
     navigate("/main");
+  };
+
+  //사용자 입력 데이터 저장
+
+  const [idValue, setId] = useState("");
+  const [pwValue, setPw] = useState("");
+
+  const saveUserId = (event) => {
+    setId(event.target.value);
+    // console.log(event.target.value);
+  };
+
+  const saveUserPw = (event) => {
+    setPw(event.target.value);
+    // console.log(event.target.value);
   };
 
   return (
@@ -20,17 +36,24 @@ const Login = () => {
             className="put"
             type="text"
             name="userName"
+            value={idValue}
             placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={saveUserId}
           ></input>
-          <input className="put" type="password" placeholder="비밀번호"></input>
-          {/* <Link to="/Main"> */}
+          <input
+            className="put"
+            type="password"
+            value={pwValue}
+            placeholder="비밀번호"
+            onChange={saveUserPw}
+          ></input>
+
           <input
             className="login_button"
             onClick={goToMain}
             type="submit"
             value="로그인"
           ></input>
-          {/* </Link> */}
         </form>
 
         <a herf="#" className="find_password">
