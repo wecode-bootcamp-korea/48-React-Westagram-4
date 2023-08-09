c75ba378cb3b0ed08ceb06f918ccb85835eef3de
wanwork <wjdqo9705@gmail.com>
2023-08-10 01:21:34 +0900

    Modify: 상수 데이터를 별도의 파일로 분리하여 동일하게 구현

--- src/pages/jeongwan/Main/Main.js
+++ src/pages/jeongwan/Main/Main.js
@@ -3,6 +3,7 @@ import "./Main.scss";
 import { useState } from "react";
 import Comment from "./components/Comment";
 import User from "./components/User";
+import { FOOTER_INFO_LIST } from "./data";
 
 const Main = () => {
   const [inputValue, setInputValue] = useState("");
@@ -273,19 +274,5 @@ const Main = () => {
     </div>
   );
 };
-
-//상수 데이터
-const FOOTER_INFO_LIST = [
-  { id: 1, text: "소개 ·", link: "https://about.instagram.com/" },
-  { id: 2, text: "도움말 ·", link: "https://about.instagram.com/" },
-  { id: 3, text: "홍보센터 ·", link: "https://about.instagram.com/" },
-  { id: 4, text: "API ·", link: "https://about.instagram.com/" },
-  { id: 5, text: "채용정보", link: "https://about.instagram.com/" },
-  { id: 6, text: "개인정보처리방침", link: "https://about.instagram.com/" },
-  { id: 7, text: "약간 ·", link: "https://about.instagram.com/" },
-  { id: 8, text: "위치 ·", link: "https://about.instagram.com/" },
-  { id: 9, text: "언어 ·", link: "https://about.instagram.com/" },
-  { id: 10, text: "Meta Verified", link: "https://about.instagram.com/" },
-];
 
 export default Main;
