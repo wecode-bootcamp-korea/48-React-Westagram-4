cd96825c3bf8c3c4dc9be6614bdb223a08bbd15a
heejii <heejii97@naver.com>
2023-08-11 11:01:04 +0900

    메인페이지 레이아웃 구현 완료

--- src/pages/heejin/Login/login.js
+++ src/pages/heejin/Login/login.js
@@ -13,57 +13,94 @@ const Login = () => {
   const [userId, setUserId] = useState("");
   const [userPw, setUserPw] = useState("");
 
-   const saveUserId = (e) => {
+  const saveUserId = (e) => {
     setUserId(e.target.value);
     console.log(userId);
-   }
+  }
 
-   const saveUserPw = (e) => {
+  const saveUserPw = (e) => {
     setUserPw(e.target.value);
     console.log(userPw);
-   }
-   const isValid = userId.includes("@") && userPw.length >= 5
-   
+  }
+  const isValid = userId.includes("@") && userPw.length >= 5
 
 
 
-  const goToMain = () => {
-    navigate('/heejin-main');
+
+  const goToMain = (e) => {
+
+    e.preventDefault();
+
+    fetch("http://10.58.52.147:3000/users/login",{
+      method:"POST",
+      headers:{
+        'Content-Type': "application/json;charset=utf-8"
+      },
+      body: JSON.stringify({
+        email:userId,
+        password:userPw
+      })
+    })
+    .then((response)=> response.json())
+    .then((result) => {
+      if (result.message === "login success") {
+        localStorage.setItem("TOKEN", result.accessToken);
+        alert("로그인 성공");
+        navigate("/heejin-main");
+
+        return;
+      }
+
+      if(result.message === "invalid password"){
+        alert("비번 틀림");
+      }else if (result.message === "specified user does not exist"){
+        alert("가");
+      }
+    })
+
+
+
+
+
+
+    
   };
-  return (
-    <div className="login">
-      <div className="loginWrap">
-        <div className="logo">
-          <h1>Westagram</h1>
-        </div>
-        <form>
-          <div className="info">
-            <input 
+
+
+return (
+  <div className="login">
+    <div className="loginWrap">
+      <div className="logo">
+        <h1>Westagram</h1>
+      </div>
+      <div>
+        <div className="info">
+          <input
             id="userId"
-            type="text" 
+            type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
             onChange={saveUserId}
-            />
-            <input 
+          />
+          <input
             id="userPW"
-            type="password" 
-            placeholder="비밀번호" 
+            type="password"
+            placeholder="비밀번호"
             onChange={saveUserPw}
-            />
-            
-          </div>
-          <div className="button">
-            <button className={isValid ? 'loginBtn blue' : 'loginBtn'} onClick={goToMain}>
-              로그인
-            </button>
-          </div>
-        </form>
-        <div className="find">
-          <Link to="/Main" className="findLink">비밀번호를 잊으셨나요?</Link>
+          />
+
+        </div>
+        <div className="button">
+          <button className={isValid ? 'loginBtn blue' : 'loginBtn'} onClick={goToMain}>
+            로그인
+          </button>
         </div>
       </div>
+      <div className="find">
+        <Link to="/Main" className="findLink">비밀번호를 잊으셨나요?</Link>
+      </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default Login;
--- src/pages/heejin/Main/main.js
+++ src/pages/heejin/Main/main.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import "../../../styles/reset.scss";
+import "../../../styles/common.scss";
 import "./Main.scss";
 
 const Main = () => {
@@ -36,25 +37,25 @@ const Main = () => {
       </header>
 
       <main>
-          <article className="feed">
-            <div className="feedTop">
-              <div className="profile">
+          <article className="feed clearFix">
+            <div className="feedTop clearFix">
+              <div className="user">
                 <img src="images/heejin/wecode_logo.jpeg" art="" />
                 <p className="bold">canon_mj</p>
               </div>
 
               <div className="moreBtn">
-                <span></span>
-                <span></span>
-                <span></span>
+                <span className="btn"></span>
+                <span className="btn"></span>
+                <span className="btn"></span>
               </div>
             </div>
 
             <div className="feedImg">
-              <img src="images/dessert_1.jpeg" art="" />
+              <img src="images/heejin/dessert_1.jpeg" art="" />
             </div>
 
-            <div className="feedIcon">
+            <div className="feedIcon clearfix">
               <div className="left">
                 <a href="#">
                   <img src="images/heejin/heart_full_icon.png" alt="좋아요 아이콘" />
@@ -74,8 +75,8 @@ const Main = () => {
               </div>
             </div>
 
-            <div className="feedContent">
-              <div className="like">
+            <div className="feedContent clearfix">
+              {/* <div className="like">
                 <img src="images/wecode_logo.jpeg" alt="" />
                 <p>
                   <span className="bold">aineworld</span>님
@@ -100,69 +101,22 @@ const Main = () => {
               <div className="feedBottom">
                 <p className="gray">댓글 달기...</p>
                 <p>게시</p>
-              </div>
+              </div> */}
+              
+                <input 
+                id="comment"
+                type="text"
+                placeholder="댓글달기 ..."
+                />
+                <button>게시</button>
+              
             </div>
 
           </article>
 
-        <div className="mainRight">
-          <div className="myProfile">
-            <p>wecode_bootcamp</p>
-            <p className="gray">WeCode | 위코드</p>
-          </div>
-
-          <div className="story">
-            <p>스토리</p>
-            <a>모두 보기</a>
-            <ul>
-              <li>
-                <p>_yum_s</p>
-                <p>16분 전</p>
-              </li>
-              <li>
-                <p>drink_eat_drink</p>
-                <p>3시간 전</p>
-              </li>
-              <li>
-                <p>hyukyc</p>
-                <p>20시간 전</p>
-              </li>
-              <li>
-                <p>jminkeek</p>
-                <p>21시간 전</p>
-              </li>
-            </ul>
-          </div>
-
-          <div className="list">
-            <p>회원님을 위한 추천</p>
-            <a>모두 보기</a>
-            <ul>
-              <li>
-                <p>joaaaaahye</p>
-                <p></p>
-                <a href="#">팔로우</a>
-              </li>
-              <li>
-                <p>rampart81</p>
-                <p></p>
-                <a href="#">팔로우</a>
-              </li>
-              <li>
-                <p>shawnjjoo</p>
-                <p></p>
-                <a href="#">팔로우</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        
       </main>
-      <footer>
-        <p>
-          Instagram 정보
-        </p>
-        <p></p>
-      </footer>
+      
     </div>
   );
 };
